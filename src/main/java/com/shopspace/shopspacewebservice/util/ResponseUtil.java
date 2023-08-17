55b1392c2440fd40006539cbef45cc3e80bc0c43
Sony <jsaulhernandez98@gmail.com>
2023-08-17 10:27:29 -0600

    tag: updating response util

--- src/main/java/com/shopspace/shopspacewebservice/util/ResponseUtil.java
+++ src/main/java/com/shopspace/shopspacewebservice/util/ResponseUtil.java
@@ -24,30 +24,38 @@ public class ResponseUtil {
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
+    public static ResponseEntity<ResponseDTO> badRequest(Object response) {
+        var responseDTO = new ResponseDTO(HttpStatus.BAD_REQUEST.name(), "400", "Invalid information!", response);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    public static ResponseEntity<ResponseDTO> unauthorized(Object response) {
+        var responseDTO = new ResponseDTO(HttpStatus.UNAUTHORIZED.name(), "401", "You do not have valid authorization!", response);
+        return new ResponseEntity<>(responseDTO, HttpStatus.UNAUTHORIZED);
+    }
+
     public static ResponseEntity<ResponseDTO> notContent(Object response) {
         var responseDTO = new ResponseDTO(HttpStatus.NOT_FOUND.name(), "404", "Resource not found!", response);
         return new ResponseEntity<>(responseDTO, HttpStatus.NOT_FOUND);
     }
 
     public static ResponseEntity<ResponseDTO> conflict(Object response) {
-        var responseDTO = new ResponseDTO(HttpStatus.CONFLICT.name(), "409",
-                "The operation cannot be performed due to a conflict!", response);
+        var responseDTO = new ResponseDTO(HttpStatus.CONFLICT.name(), "409", "The operation cannot be performed due to a conflict!", response);
         return new ResponseEntity<>(responseDTO, HttpStatus.CONFLICT);
     }
 
-    public static ResponseEntity<ResponseDTO> unauthorized(Object response) {
-        var responseDTO = new ResponseDTO(HttpStatus.UNAUTHORIZED.name(), "401", "You do not have valid authorization!",
-                response);
-        return new ResponseEntity<>(responseDTO, HttpStatus.UNAUTHORIZED);
-    }
-
-    public static ResponseEntity<ResponseDTO> badRequest(Object response) {
-        var responseDTO = new ResponseDTO(HttpStatus.BAD_REQUEST.name(), "400", "Invalid information!", response);
-        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    public static ResponseEntity<ResponseDTO> preconditionFailed(Object response) {
+        var responseDTO = new ResponseDTO(HttpStatus.PRECONDITION_FAILED.name(), "412", "The server does not meet one of the preconditions that the requester put on the request header fields", response);
+        return new ResponseEntity<>(responseDTO, HttpStatus.PRECONDITION_FAILED);
     }
 
     public static ResponseEntity<ResponseDTO> internal(Object response) {
         var responseDTO = new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.name(), "500", "Internal server error!", response);
         return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static ResponseEntity<ResponseDTO> clientUnavailable(Object response, String service) {
+        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.SERVICE_UNAVAILABLE.name(), "503", "The client's service "+service+" is not available!!!", response);
+        return new ResponseEntity<>(responseDTO, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
