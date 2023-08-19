3bdf3116004294e9d883d4d388a8fed7411ad433
Sony <jsaulhernandez98@gmail.com>
2023-08-18 18:55:19 -0600

    tag: adding new files

---
+++ src/main/java/com/shopspace/shopspacewebservice/config/ConstantsShopspace.java
@@ -0,0 +1,8 @@
+package com.shopspace.shopspacewebservice.config;
+
+public class ConstantsShopspace {
+    public static final Integer TOP_PARENT_CATEGORIES_SALE = 5;//parent categories with more sale
+    public static final Integer TOP_SUB_CATEGORIES_SALE = 4;//sub categories with more sale
+    public static final Integer TOP_PRODUCTS_SALE_WEEK = 3;//product with more sale in the week
+    public static final Integer TOP_PRODUCTS_MORE_SELLING = 2;//product with more sale
+}
---
+++ src/main/java/com/shopspace/shopspacewebservice/util/ShopspaceUtil.java
@@ -0,0 +1,4 @@
+package com.shopspace.shopspacewebservice.util;
+
+public class ShopspaceUtil {
+}
