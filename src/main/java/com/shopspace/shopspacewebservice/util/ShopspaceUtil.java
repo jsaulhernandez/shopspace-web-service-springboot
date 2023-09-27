edf3a5d2baeb2a584420372a20cbcc4505640207
Sony <jsaulhernandez98@gmail.com>
2023-09-27 17:36:37 -0600

    tag: adding method to encode text

--- src/main/java/com/shopspace/shopspacewebservice/util/ShopspaceUtil.java
+++ src/main/java/com/shopspace/shopspacewebservice/util/ShopspaceUtil.java
@@ -1,4 +1,10 @@
 package com.shopspace.shopspacewebservice.util;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class ShopspaceUtil {
+    public static String encode(String value) {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
 }
