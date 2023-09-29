d1acb53341e6eb29623cce7e8b2668a4037fd467
Sony <jsaulhernandez98@gmail.com>
2023-09-29 10:46:48 -0600

    tag: adding method to get headers

--- src/main/java/com/shopspace/shopspacewebservice/util/ShopspaceUtil.java
+++ src/main/java/com/shopspace/shopspacewebservice/util/ShopspaceUtil.java
@@ -1,10 +1,28 @@
 package com.shopspace.shopspacewebservice.util;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Map;
 
 public class ShopspaceUtil {
     public static String encode(String value) {
         return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
+
+    public static MultiValueMap<String, String> getHeaders(Map<String, Collection<String>> oldHeaders) {
+        MultiValueMap<String, String> newHeaders = new LinkedMultiValueMap<>();
+
+        for (Map.Entry<String, Collection<String>> entry : oldHeaders.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue().toString().replace("[", "").replace("]", "");
+
+            newHeaders.add(key, value);
+        }
+
+        return newHeaders;
     }
 }
