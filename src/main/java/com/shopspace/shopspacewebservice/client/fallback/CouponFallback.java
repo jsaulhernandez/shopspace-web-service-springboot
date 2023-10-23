98f562949cad0250f68af8c7ce2e1d286f5879e0
Sony <jsaulhernandez98@gmail.com>
2023-10-23 11:34:57 -0600

    tag: adding method coupon fallback

--- src/main/java/com/shopspace/shopspacewebservice/client/fallback/CouponFallback.java
+++ src/main/java/com/shopspace/shopspacewebservice/client/fallback/CouponFallback.java
@@ -1,4 +1,25 @@
 package com.shopspace.shopspacewebservice.client.fallback;
 
-public class CouponFallback {
+import com.shopspace.shopspacewebservice.client.CouponClient;
+import com.shopspace.shopspacewebservice.dto.CouponDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
+
+import java.util.Optional;
+
+public class CouponFallback implements FallbackFactory<CouponClient> {
+    Logger logger = LoggerFactory.getLogger(CouponFallback.class);
+
+    @Override
+    public CouponClient create(Throwable cause) {
+        logger.error("An exception occurred when calling the CouponClient", cause);
+        return new CouponClient() {
+            @Override
+            public Optional<CouponDTO> getCouponByCode(String code) {
+                logger.error("[Fallback] not call getCouponByCode");
+                return Optional.empty();
+            }
+        };
+    }
 }
